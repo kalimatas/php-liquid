1ec26c8d1dad40ca87a109732060ae72de9387dd
almost-online <almost@online.com.ua>
2019-02-08 12:53:38 +0200

    add ignore file

--- src/Liquid/Filterbank.php
+++ src/Liquid/Filterbank.php
@@ -19,26 +19,26 @@ use Liquid\Exception\WrongArgumentException;
  */
 class Filterbank
 {
-	/**
-	 * The registered filter objects
-	 *
-	 * @var array
-	 */
-	private $filters;
+    /**
+     * The registered filter objects
+     *
+     * @var array
+     */
+    private $filters;
 
-	/**
-	 * A map of all filters and the class that contain them (in the case of methods)
-	 *
-	 * @var array
-	 */
-	private $methodMap;
+    /**
+     * A map of all filters and the class that contain them (in the case of methods)
+     *
+     * @var array
+     */
+    private $methodMap;
 
-	/**
-	 * Reference to the current context object
-	 *
-	 * @var Context
-	 */
-	private $context;
+    /**
+     * Reference to the current context object
+     *
+     * @var Context
+     */
+    private $context;
 
     /**
      * Constructor
@@ -48,13 +48,13 @@ class Filterbank
      * @throws \Liquid\Exception\WrongArgumentException
      * @throws \ReflectionException
      */
-	public function __construct(Context $context)
-	{
-		$this->context = $context;
+    public function __construct(Context $context)
+    {
+        $this->context = $context;
 
-		$this->addFilter(StandardFilters::class);
-		$this->addFilter(CustomFilters::class);
-	}
+        $this->addFilter(StandardFilters::class);
+        $this->addFilter(CustomFilters::class);
+    }
 
     /**
      * Adds a filter to the bank
@@ -68,108 +68,116 @@ class Filterbank
      * @throws \Liquid\Exception\WrongArgumentException
      * @throws \ReflectionException
      */
-	public function addFilter($filter, callable $callback = null)
-	{
-		// If it is a callback, save it as it is
-		if (is_string($filter) && $callback) {
-			$this->methodMap[$this->toMapName($filter)] = $callback;
+    public function addFilter($filter, callable $callback = null)
+    {
+        // If it is a callback, save it as it is
+        if (is_string($filter) && $callback) {
+            $this->methodMap[$this->toMapName($filter)] = $callback;
 
-			return true;
-		}
+            return true;
+        }
 
-		// If the filter is a class, register all its static methods
-		if (is_string($filter) && class_exists($filter)) {
-			$reflection = new \ReflectionClass($filter);
-			foreach ($reflection->getMethods(\ReflectionMethod::IS_STATIC) as $method) {
-				$this->methodMap[$this->toMapName($method->name)] = $method->class;
-			}
+        // If the filter is a class, register all its static methods
+        if (is_string($filter) && class_exists($filter)) {
+            $reflection = new \ReflectionClass($filter);
+            foreach ($reflection->getMethods(\ReflectionMethod::IS_STATIC) as $method) {
+                $this->methodMap[$this->toMapName($method->name)] = $method->class;
+            }
 
-			return true;
-		}
+            return true;
+        }
 
-		// If it's a global function, register it simply
-		if (is_string($filter) && function_exists($filter)) {
-			$this->methodMap[$this->toMapName($filter)] = false;
+        // If it's a global function, register it simply
+        if (is_string($filter) && function_exists($filter)) {
+            $this->methodMap[$this->toMapName($filter)] = false;
 
-			return true;
-		}
+            return true;
+        }
 
-		// If it isn't an object an isn't a string either, it's a bad parameter
-		if (!is_object($filter)) {
-			throw new WrongArgumentException("Parameter passed to addFilter must be an object or a string");
-		}
+        // If it isn't an object an isn't a string either, it's a bad parameter
+        if (!is_object($filter)) {
+            throw new WrongArgumentException("Parameter passed to addFilter must be an object or a string");
+        }
 
-		// If the passed filter was an object, store the object for future reference.
-		$filter->context           = $this->context;
-		$className                 = get_class($filter);
-		$this->filters[$className] = $filter;
+        // If the passed filter was an object, store the object for future reference.
+        $filter->context           = $this->context;
+        $className                 = get_class($filter);
+        $this->filters[$className] = $filter;
 
-		// Then register all public static and not methods as filters
-		foreach (get_class_methods($filter) as $method) {
-			if (strtolower($method) === '__construct') {
-				continue;
-			}
-			$this->methodMap[$this->toMapName($method)] = $className;
-		}
+        // Then register all public static and not methods as filters
+        foreach (get_class_methods($filter) as $method) {
+            if (strtolower($method) === '__construct') {
+                continue;
+            }
+            $this->methodMap[$this->toMapName($method)] = $className;
+        }
 
-		return true;
-	}
+        return true;
+    }
 
-	/**
-	 * Invokes the filter with the given name
-	 *
-	 * @param string $name The name of the filter
-	 * @param string $value The value to filter
-	 * @param array  $args The additional arguments for the filter
-	 *
-	 * @return string
-	 */
-	public function invoke($name, $value, array $args = [])
-	{
-		// workaround for a single standard filter being a reserved keyword - we can't use overloading for static calls
-		if ($name === 'default') {
-			$name = '_default';
-		} else {
-			$name = $this->toMapName($name);
-		}
+    /**
+     * Invokes the filter with the given name
+     *
+     * @param string $name The name of the filter
+     * @param string $value The value to filter
+     * @param array  $args The additional arguments for the filter
+     *
+     * @return string
+     */
+    public function invoke($name, $value, array $args = [])
+    {
+        // workaround for a single standard filter being a reserved keyword - we can't use overloading for static calls
+        if ($name === 'default') {
+            $name = '_default';
+        }
 
-		array_unshift($args, $value);
+        $alias = $this->toMapName($name);
 
-		// Consult the mapping
-		if (!isset($this->methodMap[$name])) {
-			return $value;
-		}
+        array_unshift($args, $value);
 
-		$class = $this->methodMap[$name];
+        // Consult the mapping
+        if (!isset($this->methodMap[$alias])) {
+            return $value;
+        }
 
-		// If we have a callback
-		if (is_callable($class)) {
-			return call_user_func_array($class, $args);
-		}
+        $class = $this->methodMap[$alias];
 
-		// If we have a registered object for the class, use that instead
-		if (isset($this->filters[$class])) {
-			$class = $this->filters[$class];
-		}
+        // If we have a callback
+        if (is_callable($class)) {
+            return call_user_func_array($class, $args);
+        }
 
-		// If we're calling a function
-		if ($class === false) {
-			return call_user_func_array($name, $args);
-		}
+        // If we have a registered object for the class, use that instead
+        if (isset($this->filters[$class])) {
+            $class = $this->filters[$class];
+        }
 
-		// Call a class or an instance method
-		return call_user_func_array([$class, $name], $args);
-	}
+        // If we're calling a function
+        if ($class === false) {
+            if (is_callable($alias)) {
+                return call_user_func_array($alias, $args);
+            }
 
-	/**
-	 * Convert method name to camelCase, PSR-2
-	 *
-	 * @param string $method
-	 *
-	 * @return string
-	 */
-	protected function toMapName(string $method): string
-	{
-		return strtolower(str_replace('_', '', $method));
-	}
+            return call_user_func_array($name, $args);
+        }
+
+        // Call a class or an instance method
+        if (is_callable([$class, $alias])) {
+            return call_user_func_array([$class, $alias], $args);
+        }
+
+        return call_user_func_array([$class, $name], $args);
+    }
+
+    /**
+     * Convert method name to camelCase, PSR-2
+     *
+     * @param string $method
+     *
+     * @return string
+     */
+    protected function toMapName(string $method): string
+    {
+        return strtolower(str_replace('_', '', $method));
+    }
 }
