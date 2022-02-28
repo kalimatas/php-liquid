b522fbdf90a7ba50274119e6b2e2b7a5241c9233
nixx <nixx.dj@gmail.com>
2022-02-28 18:51:31 +0300

    fix parse exception "tag was never closed" with line break

--- tests/Liquid/Tag/TagIfTest.php
+++ tests/Liquid/Tag/TagIfTest.php
@@ -231,6 +231,13 @@ class TagIfTest extends TestCase
 
 		$this->assertTemplateResult('', '{% if jerry == 1 %}');
 	}
+	public function testSyntaxErrorNotClosedLineBreak()
+	{
+		$this->expectException(\Liquid\Exception\ParseException::class);
+		$this->expectExceptionMessage('if tag was never closed');
+
+		$this->assertTemplateResult('', "{% if jerry\n == 1 %}");
+	}
 
 	/**
 	 */
